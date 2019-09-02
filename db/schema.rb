9bc52d7ee1f60091268bf45a221afd68909ad640
Pumpkyns <alexy.wehrey@gmail.com>
2019-09-02 15:32:58 +0200

    ?

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 2019_09_02_124015) do
+=======
+<<<<<<< HEAD
+ActiveRecord::Schema.define(version: 2019_09_02_094831) do
+=======
+ActiveRecord::Schema.define(version: 2019_09_02_115826) do
+>>>>>>> delevery
+>>>>>>> delevery
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+<<<<<<< HEAD
+  create_table "carts", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_carts_on_item_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+=======
+<<<<<<< HEAD
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 8, scale: 2
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+=======
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+>>>>>>> delevery
+>>>>>>> delevery
+  end
+
+end
