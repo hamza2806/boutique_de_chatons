8b8682dd0292195c55cb00a64cb96fdcbe3120fe
Hamza <h.usma-2806@hotmail.com>
2019-09-02 16:16:58 +0200

    modify: rollback create_table_items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Item.destroy_all
+Cart.destroy_all
+Order.destroy_all
+User.destroy_all
+
