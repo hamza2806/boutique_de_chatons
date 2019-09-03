e883ebb4c3bf881aee8bb18f9e8515348054a36a
Hamza <h.usma-2806@hotmail.com>
2019-09-03 16:20:45 +0200

    binding stripe to orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+
+  def show
+  	@order = Order.find(params[:id])
+  end
+
+  def new
+  	@order = Order.new
+  end
+  def create
+  	@order = Order.new(user: current_user)
+  	@order.items = current_user.cart.items
+  	@amount = current_user.cart.total_price*100
+
+           customer = Stripe::Customer.create({
+                                                email: params[:stripeEmail],
+                                                source: params[:stripeToken],
+           })
+
+           charge = Stripe::Charge.create({
+                                            customer: customer.id,
+                                            amount: @amount,
+                                            description: 'Rails Stripe customer',
+                                            currency: 'eur',
+           })
+    
+      @order.save
+      current_user.orders << @order
+      flash[:success] = "You payment has been successfully processed, you will receve a confirmation email"
+      redirect_to order_path(@order.id)        
+    
+
+     rescue Stripe::CardError => e
+     flash[:error] = e.message
+     redirect_to event_path(@event.id)
+  end
+end
