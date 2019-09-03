4abe9925e09b90ddb4273a17a9eba58c1d5e29bf
AliZainoul <ali.zainoul.az@gmail.com>
2019-09-03 18:02:16 +0200

    [CREATE] profiles_controller.rb and views/profiles

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :orders
 
   resources :items
-
+  get '/profiles/:id', to: 'profiles#show', as: 'profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
