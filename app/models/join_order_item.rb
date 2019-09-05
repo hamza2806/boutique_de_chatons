fe8faf96fd0462783eb00ea886b9d8ec55d710b9
Hamza <h.usma-2806@hotmail.com>
2019-09-05 20:31:01 +0200

    fix mailer

--- app/models/join_order_item.rb
+++ app/models/join_order_item.rb
@@ -2,7 +2,7 @@ class JoinOrderItem < ApplicationRecord
 	belongs_to :item 
 	belongs_to :order
 
-	after_create :confirmation_send
+	# after_create :confirmation_send
 
     def confirmation_send
         AdminMailer.info_order(self).deliver_now
