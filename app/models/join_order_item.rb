17426cb29dea74239e03d2fd320e04e761a56922
roman <roman.c6lle@gmail.com>
2019-09-05 17:02:37 +0200

    resolve mailer

--- app/models/join_order_item.rb
+++ app/models/join_order_item.rb
@@ -1,4 +1,10 @@
 class JoinOrderItem < ApplicationRecord
 	belongs_to :item 
 	belongs_to :order
+
+	after_create :confirmation_send
+
+    def confirmation_send
+        AdminMailer.info_order(self).deliver_now
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_103625) do
+ActiveRecord::Schema.define(version: 2019_09_05_093058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_103625) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "total_price"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
